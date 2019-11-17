e45c110b0831a92427c086717791d9bd4153e3cc
Ryan Cummins <brokenrhino@gmail.com>
2019-11-17 14:31:59 -0500

    Made the size squares adjust to the width of the red square.

--- EasyAnchors/EasyAnchors/ViewController.swift
+++ EasyAnchors/EasyAnchors/ViewController.swift
@@ -24,9 +24,12 @@ class ViewController: UIViewController {
     
     [redView, blueView, greenView].forEach { view.addSubview($0) }
         
-    redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 12), size: .init(width: 100, height: 100))
+    redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 12), size: .init(width: 125, height: 0))
+    redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
     
-    blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0), size: .init(width: 100, height: 100))
+    blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 12, left: 0, bottom: 0, right: 0))
+    blueView.widthAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+    blueView.heightAnchor.constraint(equalTo: blueView.widthAnchor).isActive = true
     
     greenView.anchor(top: redView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 12, bottom: 0, right: 12))
     
