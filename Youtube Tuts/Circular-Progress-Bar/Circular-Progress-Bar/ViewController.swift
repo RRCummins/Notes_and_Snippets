06ef179b6ec88eb82e7b8eb77bee447ee5a4193f
Ryan Cummins <brokenrhino@gmail.com>
2019-12-12 00:31:20 -0500

    More of the pulsating animation is complete

--- Youtube Tuts/Circular-Progress-Bar/Circular-Progress-Bar/ViewController.swift
+++ Youtube Tuts/Circular-Progress-Bar/Circular-Progress-Bar/ViewController.swift
@@ -23,11 +23,18 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     label.text = "Start"
     label.textAlignment = .center
     label.font = UIFont.boldSystemFont(ofSize: 32)
+    label.textColor = .white
     return label
   }()
   
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
   let shapeLayer = CAShapeLayer()
   let trackPath = CAShapeLayer()
+  var pulsatingLayer: CAShapeLayer!
+  
   var centerPoint: CGPoint = CGPoint(x: 0, y: 0)
   var baseColor: CGColor = UIColor.lightGray.cgColor
   var topColor: CGColor = UIColor.red.cgColor
@@ -35,13 +42,15 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
   var lineWidth: CGFloat = 10
   var radius: CGFloat = 100
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
+  fileprivate func addPercentageLabel() {
     view.addSubview(percentageLabel)
     percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
     percentageLabel.center = view.center
-    
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .backgroundColor
     centerPoint = view.center
     view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     
@@ -49,7 +58,6 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
   }
   
   func begindDownloadingFile() {
-    
     shapeLayer.strokeEnd = 0
   
     let configuration = URLSessionConfiguration.default
@@ -68,10 +76,8 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
       DispatchQueue.main.async {
         self.percentageLabel.text = "\(Int(percentage * 100))%"
         self.shapeLayer.strokeEnd = percentage
-        
       }
-      
-      print(percentage)
+//      print(percentage)
     }
     
     
@@ -86,36 +92,65 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
   }
 
    private func setUp() {
-     addTrackPath(center: centerPoint)
-     addAnimatedPath(center: centerPoint)
+    addPulsatingLayer(center: centerPoint)
+    addTrackPath(center: centerPoint)
+    addAnimatedPath(center: centerPoint)
+    
+    addPercentageLabel()
    }
 
-   func addTrackPath(center: CGPoint) {
+   func addPulsatingLayer(center: CGPoint) {
+    pulsatingLayer = CAShapeLayer()
     let circularPath = UIBezierPath(arcCenter: .zero, radius: radius, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
-     trackPath.path = circularPath.cgPath
-     trackPath.strokeColor = baseColor
-     trackPath.lineWidth = lineWidth
-     trackPath.fillColor = UIColor.clear.cgColor
-     trackPath.lineCap = CAShapeLayerLineCap.round
-     trackPath.position = view.center
+    pulsatingLayer.path = circularPath.cgPath
+    pulsatingLayer.strokeColor = UIColor.clear.cgColor
+    pulsatingLayer.lineWidth = lineWidth
+    pulsatingLayer.fillColor = UIColor.pulsatingFillColor.cgColor
+    pulsatingLayer.lineCap = CAShapeLayerLineCap.round
+    pulsatingLayer.position = view.center
      
-    view.layer.addSublayer(trackPath)
+    view.layer.addSublayer(pulsatingLayer)
+    
+    animatePulsatingLayer()
    }
+  
+  func addTrackPath(center: CGPoint) {
+    let circularPath = UIBezierPath(arcCenter: .zero, radius: radius, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+    trackPath.path = circularPath.cgPath
+    trackPath.strokeColor = UIColor.trackStrokeColor.cgColor
+    trackPath.lineWidth = 20
+    trackPath.fillColor = UIColor.backgroundColor.cgColor
+    trackPath.lineCap = CAShapeLayerLineCap.round
+    trackPath.position = view.center
+    
+   view.layer.addSublayer(trackPath)
+  }
    
    func addAnimatedPath(center: CGPoint) {
     let circularPath = UIBezierPath(arcCenter: .zero, radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-     shapeLayer.path = circularPath.cgPath
-     shapeLayer.strokeColor = topColor
-     shapeLayer.lineWidth = lineWidth
-     shapeLayer.fillColor = UIColor.clear.cgColor
-     shapeLayer.strokeEnd = 0
-     shapeLayer.lineCap = CAShapeLayerLineCap.round
-     shapeLayer.position = view.center
+    shapeLayer.path = circularPath.cgPath
+    shapeLayer.strokeColor = UIColor.outlineStrokeColor.cgColor
+    shapeLayer.lineWidth = 20
+    shapeLayer.fillColor = UIColor.clear.cgColor
+    shapeLayer.strokeEnd = 0
+    shapeLayer.lineCap = CAShapeLayerLineCap.round
+    shapeLayer.position = view.center
     
     shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
      
     view.layer.addSublayer(shapeLayer)
    }
+  
+  func animatePulsatingLayer() {
+    let animation = CABasicAnimation(keyPath: "transform.scale")
+    animation.toValue = 1.5
+    animation.duration = 0.8
+    animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+    animation.autoreverses = true
+    animation.repeatCount = Float.infinity
+    
+    pulsatingLayer.add(animation, forKey: "pulsating")
+  }
 
   
    func animateCircle() {
