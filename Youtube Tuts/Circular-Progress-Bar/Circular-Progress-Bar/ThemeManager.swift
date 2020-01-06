3398ee4fae4104a1e3eb4087337f38f306a48ec8
Ryan Cummins <brokenrhino@gmail.com>
2020-01-06 12:27:49 -0500

    Added ColorUpdatable  protocol

--- Youtube Tuts/Circular-Progress-Bar/Circular-Progress-Bar/ThemeManager.swift
+++ Youtube Tuts/Circular-Progress-Bar/Circular-Progress-Bar/ThemeManager.swift
@@ -5,6 +5,16 @@
 import UIKit
 import Foundation
 
+// A protocol which denotes types which can update their colors.
+protocol ColorUpdatable {
+
+  // The theme for which to update colors.
+  var theme: Theme { get set }
+
+  // A function that is called when colors should be updated.
+  func updateColors(for theme: Theme)
+}
+
 protocol ColorThemeObserving {
 
   // Registers observance of 'didChangeColorTheme' custom notifications.
