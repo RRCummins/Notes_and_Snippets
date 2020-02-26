b7bb10376b30a216b19267c9fede3da437efedd2
Ryan Cummins <brokenrhino@gmail.com>
2020-02-26 17:57:02 -0500

    Neuromorphic Button Pushes

--- Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
+++ Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
@@ -16,11 +16,21 @@ struct SimpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
         .padding(30)
+        .contentShape(Circle())
         .background(
-            Circle()
-                .fill(Color.offWhite)
-                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            Group {
+                if configuration.isPressed {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                } else {
+                    Circle()
+                        .fill(Color.offWhite)
+                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                        .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                }
+            }
         )
     }
 }
