c37fd72cfe3c48a460eabda31637dd0aceb5a844
Ryan Cummins <brokenrhino@gmail.com>
2020-02-26 18:41:11 -0500

    Fixed the color and added icon

--- Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
+++ Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
@@ -9,7 +9,11 @@
 import SwiftUI
 
 extension Color {
-    static let offWhite = Color(red: 225 / 55, green: 225 / 255, blue: 235 / 255)
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+    
+    static let darkStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    static let darkEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
+    
 }
 
 extension LinearGradient {
