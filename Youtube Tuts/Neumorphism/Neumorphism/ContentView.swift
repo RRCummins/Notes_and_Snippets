bf6e219fad65987c9a13fbad588e3d261b77c976
Ryan Cummins <brokenrhino@gmail.com>
2020-02-26 17:48:56 -0500

    Neumorphic Button

--- Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
+++ Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
@@ -12,20 +12,50 @@ extension Color {
     static let offWhite = Color(red: 225 / 55, green: 225 / 255, blue: 235 / 255)
 }
 
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .background(
+            Circle()
+                .fill(Color.offWhite)
+                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+        )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         ZStack {
             Color.offWhite
             
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.offWhite)
-                .frame(width: 300, height: 300)
-                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            Button(action: {
+                print("Button Tapped")
+            }) {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.gray)
+            }
+        .buttonStyle(SimpleButtonStyle())
         }
         .edgesIgnoringSafeArea(.all)
     }
 }
+
+//struct ContentView: View {
+//    var body: some View {
+//        ZStack {
+//            Color.offWhite
+//
+//            RoundedRectangle(cornerRadius: 25)
+//                .fill(Color.offWhite)
+//                .frame(width: 300, height: 300)
+//                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+//                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+//        }
+//        .edgesIgnoringSafeArea(.all)
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
