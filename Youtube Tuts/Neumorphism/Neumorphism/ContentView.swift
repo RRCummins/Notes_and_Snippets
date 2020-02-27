48c9846893f4ebba3b4b6b17cb507cb68ccdcf66
Ryan Cummins <brokenrhino@gmail.com>
2020-02-26 21:51:53 -0500

    Rounded Button

--- Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
+++ Youtube Tuts/Neumorphism/Neumorphism/ContentView.swift
@@ -22,6 +22,26 @@ extension LinearGradient {
     }
 }
 
+struct DarkBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+    
+    var body: some View {
+        ZStack { if isHighlighted {
+            shape
+                .fill(LinearGradient(Color.darkEnd, Color.darkStart))
+                .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+        } else {
+            shape
+                .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+            }
+        }
+    }
+}
+
 struct SimpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
@@ -54,26 +74,6 @@ struct SimpleButtonStyle: ButtonStyle {
                 }
             }
         )
-    }
-}
-
-struct DarkBackground<S: Shape>: View {
-    var isHighlighted: Bool
-    var shape: S
-    
-    var body: some View {
-        ZStack { if isHighlighted {
-                shape
-                    .fill(Color.darkEnd)
-                    .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
-                    .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
-            } else {
-                shape
-                    .fill(Color.darkEnd)
-                    .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
-                    .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
-            }
-        }
     }
 }
 
