1600e6bc3d3cb053e5db1468b716ecbcf8acf6ca
Ryan Cummins <brokenrhino@gmail.com>
2020-03-04 00:13:26 -0500

    Even more

--- Youtube Tuts/Project1SwiftUI/Project1SwiftUI/ContentView.swift
+++ Youtube Tuts/Project1SwiftUI/Project1SwiftUI/ContentView.swift
@@ -6,11 +6,20 @@
 //  Copyright © 2020 Them Rhinos. All rights reserved.
 //
 
+import Combine
 import SwiftUI
+
+class DataSource: ObservableObject {
+    let objectWillChange = PassthroughSubject<Void, Never>()
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Text("Hello, World!")
+            }.navigationBarTitle("Storm Viewer")
+        }
     }
 }
 
