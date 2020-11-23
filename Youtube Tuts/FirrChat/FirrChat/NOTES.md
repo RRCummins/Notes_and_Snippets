0a3ed625ff551a5ec55dfa8545da1d9fae784754
Ryan Cummins <brokenrhino@gmail.com>
2020-11-23 17:02:11 -0500

    FirrChat - Initial Commit

---
+++ Youtube Tuts/FirrChat/FirrChat/NOTES.md
@@ -0,0 +1,7 @@
+#  Notes
+
+## [Swift: Firebase Chat App Part 1 - Getting Started (Real-time) - Xcode 11 - 2020](https://www.youtube.com/watch?v=BZEluKixqDA)
+
+
+
+
---
+++ Youtube Tuts/FirrChat/FirrChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirrChat
+//
+//  Created by Ryan Cummins on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
