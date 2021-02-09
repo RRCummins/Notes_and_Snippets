367460014c09de163403d966605cbd99b5e7bb82
Ryan Cummins <brokenrhino@gmail.com>
2021-02-09 00:15:41 -0500

    Added login buttons

--- Youtube Tuts/InstaScroll/InstaScroll/Controllers/Other/Onboarding/LoginViewController.swift
+++ Youtube Tuts/InstaScroll/InstaScroll/Controllers/Other/Onboarding/LoginViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let usernameEmailField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordField: UITextField = {
+       let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        return UIButton()
+    }()
+    private let createAccountButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView: UIView = {
+        return UIView()
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        
+        addSubviews()
+        view.backgroundColor = .systemBackground
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Assign Frames
+    }
+    
+    private func addSubviews() {
+        [usernameEmailField,
+                 passwordField,
+                 loginButton,
+                 termsButton,
+                 privacyButton,
+                 createAccountButton,
+                 headerView].forEach {view.addSubview($0)}
+    }
+    
+    @objc private func didTapLoginButton() {}
+    
+    @objc private func didTapTermsButton() {}
+    
+    @objc private func didTapPrivacyButton() {}
+    
+    @objc private func didTapCreateAccountButton() {}
+    
+    
     
 
 }
