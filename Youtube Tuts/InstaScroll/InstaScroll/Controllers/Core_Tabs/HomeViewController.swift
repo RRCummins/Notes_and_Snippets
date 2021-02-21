4fc7dc9bfcd4e7972dd3b879081f50792de96499
Ryan Cummins <brokenrhino@gmail.com>
2021-02-21 13:08:09 -0500

    Setup home feed wireframe and models; Finished part 14

--- Youtube Tuts/InstaScroll/InstaScroll/Controllers/Core_Tabs/HomeViewController.swift
+++ Youtube Tuts/InstaScroll/InstaScroll/Controllers/Core_Tabs/HomeViewController.swift
@@ -20,7 +20,7 @@ class HomeViewController: UIViewController {
     private var feedRenderedModels = [HomeFeedRenderViewModel]()
     
     private let tableView: UITableView = {
-       let tv = UITableView()
+        let tv = UITableView()
         // Register Cells
         tv.register(IGFeedPostTableViewCell.self, forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
         tv.register(IGFeedPostHeaderTableViewCell.self, forCellReuseIdentifier: IGFeedPostHeaderTableViewCell.identifier)
@@ -28,12 +28,13 @@ class HomeViewController: UIViewController {
         tv.register(IGFeedPostGeneralTableViewCell.self, forCellReuseIdentifier: IGFeedPostGeneralTableViewCell.identifier)
         return tv
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
+        createMockModels()
     }
     
     override func viewDidLayoutSubviews() {
@@ -47,6 +48,46 @@ class HomeViewController: UIViewController {
         
     }
     
+    private func createMockModels() {
+        let user = User(userName: "joe",
+                        bio: "",
+                        name: (first: "", last: ""),
+                        profilePhotoURL: URL(string: "https://www.google.com")!,
+                        birthDate: Date(),
+                        gender: .male,
+                        counts: UserCount(followers: 1, following: 1, posts: 1),
+                        joinedDate: Date())
+        let post = UserPost(identifier: "123",
+                            postType: .photo,
+                            thumbnailImage: URL(string: "https://www.google.com")!,
+                            postURL: URL(string: "https://www.google.com")!,
+                            caption: nil,
+                            likeCount: [],
+                            comments: [],
+                            createdDate: Date(),
+                            taggedUsers: [],
+                            owner: user)
+        var comments = [PostComment]()
+        for x in 0..<2 {
+            comments.append(PostComment(
+                                identifier: "789_\(x)",
+                                username: "@jenny\(x)",
+                                text: "The best post I've seen",
+                                createdDate: Date(),
+                                likes: [])
+            )
+        }
+        
+        for x in 0..<5 {
+            let viewModel = HomeFeedRenderViewModel(
+                header: PostRenderViewModel(renderType: .header(provider: user)),
+                post: PostRenderViewModel(renderType: .primaryContent(provider: post)),
+                actions: PostRenderViewModel(renderType: .actions(provider: "")),
+                comments: PostRenderViewModel(renderType: .comments(comment: comments)))
+            feedRenderedModels.append(viewModel)
+        }
+    }
+    
     func handleNotAuthenticated() {
         // Check auth status
         if Auth.auth().currentUser == nil {
@@ -56,24 +97,134 @@ class HomeViewController: UIViewController {
             present(loginVC, animated: false)
         }
     }
-
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
-        
-        return cell
+        return feedRenderedModels.count * 4
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let x = section
+        let model: HomeFeedRenderViewModel
+        if x == 0 {
+            model = feedRenderedModels[0]
+        }
+        else {
+            let position = x % 4 == 0 ? x/4 : ((x - (x % 4)) / 4)
+            model = feedRenderedModels[position]
+        }
+        
+        let subSection = x % 4
+        
+        if subSection == 0 {
+            // Header
+            return 1
+        }
+        else if subSection == 1 {
+            // Post
+            return 1
+        }
+        else if subSection == 2 {
+            // actions
+            return 1
+        }
+        else if subSection == 3 {
+            // Comments
+            let commentModel = model.comments
+            switch commentModel.renderType {
+            case .comments(let comments): return comments.count > 2 ? 2 : comments.count
+            case .header, .actions, .primaryContent: return 0
+            }
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let x = indexPath.section
+        let model: HomeFeedRenderViewModel
+        if x == 0 {
+            model = feedRenderedModels[0]
+        }
+        else {
+            let position = x % 4 == 0 ? x/4 : ((x - (x % 4)) / 4)
+            model = feedRenderedModels[position]
+        }
+        let subSection = x % 4
+        
+        if subSection == 0 {
+            // Header
+            switch model.header.renderType {
+            case .header(let user):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier, for: indexPath) as! IGFeedPostHeaderTableViewCell
+                return cell
+            case .comments, .actions, .primaryContent: return UITableViewCell()            }
+        }
+        else if subSection == 1 {
+            // Post
+            switch model.post.renderType {
+            case .primaryContent(let post):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+                return cell
+            case .header, .actions, .comments: return UITableViewCell()
+            }
+        }
+        else if subSection == 2 {
+            // actions
+            switch model.actions.renderType {
+            case .actions(let provider):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier, for: indexPath) as! IGFeedPostActionsTableViewCell
+                return cell
+            case .comments, .header, .primaryContent: return UITableViewCell()             }
+        }
+        else if subSection == 3 {
+            // Comments
+            switch model.comments.renderType {
+            case .comments(let comments):
+                let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostGeneralTableViewCell.identifier, for: indexPath) as! IGFeedPostGeneralTableViewCell
+                return cell
+            case .header, .actions, .primaryContent: return UITableViewCell()             }
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let subSection = indexPath.section % 4
+        if subSection == 0 {
+            // Header
+            return 70
+        }
+        else if subSection == 1 {
+            // Post
+            return tableView.width
+        }
+        else if subSection == 2 {
+            // Actions
+            return 60
+        }
+        else if subSection == 3 {
+            // Comments
+            return 50
+        }
+        else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        let subSection = section % 4
+        return subSection == 3 ? 70 : 0
+    }
     
 }
 
