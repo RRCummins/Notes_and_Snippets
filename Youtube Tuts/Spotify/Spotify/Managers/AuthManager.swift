5e6598808a5f0c8fdaf7fd8ed2ac05e93851da3e
Ryan Cummins <brokenrhino@gmail.com>
2021-02-22 20:58:17 -0500

    Added WebView for Auth with Spotify; Finished Part 2

--- Youtube Tuts/Spotify/Spotify/Managers/AuthManager.swift
+++ Youtube Tuts/Spotify/Spotify/Managers/AuthManager.swift
@@ -10,7 +10,20 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "27551f79e2404cb6a6721f5369c3b540"
+        static let clientSecret = "800333e3924b4175bb90106747b76d83"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -24,11 +37,26 @@ final class AuthManager {
         return nil
     }
     
-    private var tokehExpirationDate: Date? {
+    private var tokenExpirationDate: Date? {
         return nil
     }
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get Token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    public func cacheToken() {
+        
     }
 }
