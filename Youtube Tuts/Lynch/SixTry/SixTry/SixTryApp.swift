d4c6dc8a8f1e55614e36d5e232fc931f434f70fc
Ryan Cummins <brokenrhino@gmail.com>
2022-03-09 18:24:03 -0500

    Changes

---
+++ Youtube Tuts/Lynch/SixTry/SixTry/SixTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixTryApp.swift
+//  SixTry
+//
+//  Created by Ryan Cummins on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SixTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
