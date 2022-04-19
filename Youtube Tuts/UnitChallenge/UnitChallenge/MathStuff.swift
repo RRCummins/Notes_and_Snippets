75b3df3c350a12bde08861e4ce6bf0c6b7c34988
Ryan Cummins <brokenrhino@gmail.com>
2022-04-19 18:23:11 -0400

    Cleanup

---
+++ Youtube Tuts/UnitChallenge/UnitChallenge/MathStuff.swift
@@ -0,0 +1,24 @@
+//
+//  MathStuff.swift
+//  UnitChallenge
+//
+//  Created by Ryan Cummins on 4/4/22.
+//
+
+import Foundation
+
+class MathStuff {
+    
+    func addNumbers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divideNumbers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+    
+}
---
+++ Youtube Tuts/UnitChallenge/UnitChallenge/UnitChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitChallengeApp.swift
+//  UnitChallenge
+//
+//  Created by Ryan Cummins on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
